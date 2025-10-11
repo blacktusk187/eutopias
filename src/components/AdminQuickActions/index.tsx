a80c2e7778006f5ed2a2852e3947ee7af64f4455
blacktusk187 <barnsleyt@gmail.com>
2025-10-11 10:56:24 -0700

    Improve AdminQuickActions styling and structure
    Refactored AdminQuickActions to wrap content in a dedicated div and imported the SCSS file in the component. Updated SCSS to remove list bullets, set transparent group heading backgrounds, and reset list margins and paddings for a cleaner UI.

--- src/components/AdminQuickActions/index.tsx
+++ src/components/AdminQuickActions/index.tsx
@@ -3,33 +3,36 @@
 import React from 'react'
 import { NavGroup } from '@payloadcms/ui'
 import Link from 'next/link'
+import './index.scss'
 
 const AdminQuickActions: React.FC = () => {
   return (
-    <NavGroup label="Quick actions">
-      <ul className="nav-group__list">
-        <li className="nav__item">
-          <Link className="nav__link" href="/admin/collections/posts/create">
-            Create a new Post
-          </Link>
-        </li>
-        <li className="nav__item">
-          <Link className="nav__link" href="/admin/collections/pages/create">
-            Create a new Page
-          </Link>
-        </li>
-        <li className="nav__item">
-          <Link className="nav__link" href="/admin/collections/media">
-            Manage Media
-          </Link>
-        </li>
-        <li className="nav__item">
-          <a className="nav__link" href="/" target="_blank" rel="noopener noreferrer">
-            View Site
-          </a>
-        </li>
-      </ul>
-    </NavGroup>
+    <div className="admin-quick-actions">
+      <NavGroup label="Quick actions">
+        <ul className="nav-group__list">
+          <li className="nav__item">
+            <Link className="nav__link" href="/admin/collections/posts/create">
+              Create a new Post
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link className="nav__link" href="/admin/collections/pages/create">
+              Create a new Page
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link className="nav__link" href="/admin/collections/media">
+              Manage Media
+            </Link>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="/" target="_blank" rel="noopener noreferrer">
+              View Site
+            </a>
+          </li>
+        </ul>
+      </NavGroup>
+    </div>
   )
 }
 
