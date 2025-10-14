d5bc2ab38e89ec04be7a8b0cafe858bb1b6791c5
blacktusk187 <barnsleyt@gmail.com>
2025-10-13 20:14:29 -0700

    Add newsletter subscription with Resend integration
    Implements a newsletter subscription form and backend using Resend. Adds new API routes for subscribing users and sending emails, integrates the 'resend' and 'sonner' packages, and updates the newsletter page to include the subscription form. Also introduces a basic email template and notification toasts for user feedback.

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,15 @@
+'use client'
+
 import React from 'react'
+import { Toaster } from 'sonner'
 
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      {children}
+      <Toaster position="bottom-right" richColors closeButton />
+    </>
+  )
 }
